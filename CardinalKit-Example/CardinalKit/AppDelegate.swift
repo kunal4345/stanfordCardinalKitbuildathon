d478a72f187bdc2da3b2a2c15a0d6c78592fd4a8
IsabelSong <hjsonghjsong@gmail.com>
2020-10-10 21:30:44 +0900

    Added OCKTask

--- CardinalKit-Example/CardinalKit/AppDelegate.swift
+++ CardinalKit-Example/CardinalKit/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import Firebase
 import ResearchKit
 import CareKit //hjsong
+import CareKitStore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -76,4 +77,48 @@ extension AppDelegate {
 
 
 
+//hjsong
+extension OCKStore {
+
+    // Adds tasks and contacts into the store
+    func populateSampleData() {
+
+       
+        let thisMorning = Calendar.current.startOfDay(for: Date())
+        let aFewDaysAgo = Calendar.current.date(byAdding: .day, value: -4, to: thisMorning)!
+        let beforeBreakfast = Calendar.current.date(byAdding: .hour, value: 8, to: aFewDaysAgo)!
+        let afterLunch = Calendar.current.date(byAdding: .hour, value: 14, to: aFewDaysAgo)!
+
+        let schedule = OCKSchedule(composing: [
+            OCKScheduleElement(start: beforeBreakfast, end: nil,
+                               interval: DateComponents(day: 1)),
+
+            OCKScheduleElement(start: afterLunch, end: nil,
+                               interval: DateComponents(day: 2))
+        ])
+
+        
+        var doxylamine = OCKTask(id: "doxylamine", title: "Brace",
+                                 carePlanUUID: nil, schedule: schedule)
+        doxylamine.instructions = "Wear for as long as desired"
+
+
+        let nauseaSchedule = OCKSchedule(composing: [OCKScheduleElement(start: beforeBreakfast, end: nil, interval: DateComponents(day: 2))])
+        var nausea = OCKTask(id: "nausea", title: "Breathe",
+                             carePlanUUID: nil, schedule: nauseaSchedule)
+        nausea.impactsAdherence = false
+        nausea.instructions = "\u{2022} Take a slow breath in through your nose, breathing into your lower belly (for about 4 seconds).\n\n\u{2022} Hold for 1 to 2 seconds.\n\n\u{2022}Exhale slowly through your mouth (for about 4 seconds).\n\n\u{2022} Wait a few seconds before taking another breath."
+
+        
+        let surveySchedule = OCKSchedule(composing: [OCKScheduleElement(start: beforeBreakfast, end: nil, interval: DateComponents(day: 2))])
+        var survey=OCKTask(id: "survey", title: "Pain", carePlanUUID: nil, schedule: surveySchedule)
+        survey.instructions = "Back Pain"
+        
+
+        addTasks([nausea, doxylamine], callbackQueue: .main, completion: nil)
+        addTasks([survey], callbackQueue: .main, completion: nil)
+
+  
+}
+}
 
